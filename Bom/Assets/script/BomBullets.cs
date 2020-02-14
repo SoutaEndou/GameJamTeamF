5dd2dbc0141a607cb94199ba9652364523c58bfc
popo8 <cop0314@gmail.com>
2020-02-14 16:11:28 +0900

    unityプロジェクトの追跡

---
+++ Bom/Assets/script/BomBullets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomBullets : MonoBehaviour
+{
+    float delta;
+    public float advanceBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta = 0.0f;
+        advanceBullet = -0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        transform.Translate(0, 0, this.advanceBullet);
+        if (delta >= 2.0f)
+        {
+            delta = 0.0f;
+            Destroy(gameObject);
+        }
+    }
+}
